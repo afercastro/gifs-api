9ed647eb6eb64610b11eb396b428593ac0d6ebf9
Andres Fernandez Castro <afcastro.ssipp@gmail.com>
2020-07-29 21:34:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import './App.css';
+import ListOfGifs from './components/ListOfGifs';
+
+function App() {
+  const [keyword, setKeyword] = useState('panda')
+  return (
+    <div className="App">
+      <section className="App-content">
+      <button onClick={()=>setKeyword('mapache')}
+        >Cambiar keyword</button>
+        <ListOfGifs keyword={keyword}/> 
+      </section>
+    </div>
+  );
+}
+
+export default App;
